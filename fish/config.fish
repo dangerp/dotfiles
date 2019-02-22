70d0abd0d7bdc0ad526b20a3162e0a66131e3aec
Justin Powers <justin.powers@salesforce.com>
2019-02-22 07:01:31 -0800

    Change git config to Justin

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set -g fish_greeting ''
 
+<<<<<<< HEAD
 set -x EDITOR (which vim)
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH /usr/local/bin $PATH
@@ -10,3 +11,10 @@ set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $HOME/bin $PATH
 set -x RUBYGEMS_EC2_DB1 ec2-54-245-133-190.us-west-2.compute.amazonaws.com
 set -x RUBYGEMS_EC2_LB1 54.245.255.174
+=======
+set EDITOR /Users/justin.powers/bin/subl -w
+set PATH /usr/local/bin $PATH
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/share/npm/bin $PATH
+set PATH $HOME/bin $PATH
+>>>>>>> Change git config to Justin
